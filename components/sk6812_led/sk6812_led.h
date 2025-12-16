7abd03bdd09bff8dbb2df5611cb0fca29300b586
MaximSha <maxim.shamshin@sigur.com>
2025-12-16 21:22:23 +0300

    comments

--- components/sk6812_led/sk6812_led.h
+++ components/sk6812_led/sk6812_led.h
@@ -1,9 +1,8 @@
 #include "stdint.h"
 
-//  add to gpio.c:
+/*          add to gpio.h / gpio.c:      */
 //  esp_err_t gpio_set_level_insecure(gpio_num_t gpio_num, uint32_t level)
 //  {
-//      GPIO_CHECK((gpio_num >= 0), "GPIO output gpio_num error", ESP_ERR_INVALID_ARG);
 //      gpio_hal_set_level(gpio_context.gpio_hal, gpio_num, level);
 //      return ESP_OK;
 // }
